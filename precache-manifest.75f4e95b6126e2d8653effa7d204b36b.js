e92a583dd15e2a6352d9d20d314e5987fe20b043
unknown <Sandeep.V@yethi.in>
2020-01-15 17:27:55 +0530

    Updates

---
+++ precache-manifest.75f4e95b6126e2d8653effa7d204b36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb750f2677e818636c243e059a7df42d",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "978289abbc73adeedb85",
+    "url": "/monster-roledex/static/css/main.4b8fb662.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monster-roledex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-roledex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "978289abbc73adeedb85",
+    "url": "/monster-roledex/static/js/main.79a2d6db.chunk.js"
+  },
+  {
+    "revision": "59ec43f1f378d3a69ba4",
+    "url": "/monster-roledex/static/js/runtime-main.8845442f.js"
+  }
+]);
